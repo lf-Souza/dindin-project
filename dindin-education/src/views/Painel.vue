8aa7cbac606188dc40799892a5c8dee3c6ba1140
luiz_souza <luiz_souza@usp.br>
2021-10-19 21:30:58 -0300

    atualização do painel administrativo

--- dindin-education/src/views/Painel.vue
+++ dindin-education/src/views/Painel.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="painel">
     <h1 class="title-painel"> Painel Administrativo </h1>
-    <h1 class="subtitle-painel"> Educação financeira é tudo de bom </h1>
-    
+    <h2 class="subtitle-painel"> Educação financeira é tudo de bom </h2>
+    <div class="table-title">
+      <h3 class="title1">Curso</h3>
+      <h3 class="title2">Ações</h3>
+    </div>
   </div>
 </template>
 
@@ -13,6 +16,7 @@ export default {
 </script>
 
 <style>
+
 .title-painel {
   display: flex;
   position: absolute;
@@ -55,4 +59,64 @@ export default {
 
   color: #555555;
 }
+
+.table-title {
+  display: flex;
+  position: absolute;
+  flex-direction: row;
+  align-items: center;
+  width: 812px;
+  height: 44px;
+  left: 82px;
+  top: 397px;
+
+  background: #EBEBEB;
+  border: 1px solid #989898;
+  box-sizing: border-box;
+}
+
+.title1 {
+  display: flex;
+  position: absolute;
+  flex-direction: row;
+  align-items: center;
+  width: 63px;
+  height: 28px;
+  left: 24px;
+  top: 8px;
+
+  font-family: 'Ubuntu', sans-serif;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 24px;
+  line-height: 28px;
+  /* identical to box height */
+
+  text-transform: capitalize;
+
+  color: #000000;
+}
+
+.title2 {
+  display: flex;
+  position: absolute;
+  flex-direction: row;
+  align-items: center;
+  width: 65px;
+  height: 28px;
+  left: 457px;
+  top: 8px;
+
+  font-family: Ubuntu;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 24px;
+  line-height: 28px;
+  /* identical to box height */
+
+  text-transform: capitalize;
+
+  color: #000000;
+}
+
 </style>
