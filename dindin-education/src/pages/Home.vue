b73744ca27f9bf4f560be0f42b50777dbcfea229
luiz_souza <luiz_souza@usp.br>
2021-10-21 11:07:22 -0300

    Atualização da page home

--- dindin-education/src/pages/Home.vue
+++ dindin-education/src/pages/Home.vue
@@ -1,67 +1,67 @@
 <template>
-  <div div="curso-home">
-    <h1>Cursos</h1>
-    <div class="card_curso">
-      <b-card
-        title="Investimento para Iniciantes"
-        img-src="../assets/image3.png"
+  <div class="curso-home">
+     
+      <div div id="home">
+        <img class="dindin-logo" 
+        alt="Pagina Inicial" src="../assets/dindin-logo-white.png"
         img-left
-        tag="article"
         style="max-width: 20rem;"
-        class="mb-2"
-      >
+        >
+      
         <b-card-text>
-          Some quick example text to build on the card title and make up the bulk of the card's content.
+         <p> Bem-vindos à  DinDin! Aqui você encontra o melhor conteúdo sobre educação financeira.
+          Veja nossos cursos e conte conosco para o que precisar. </p>
         </b-card-text>
-        <b-button href="#" variant="success">ComeÃ§ar Agora</b-button>
-      </b-card>
-    </div>
-
-    <div class="card_curso">
-      <b-card
-        title="Poupando e rendendo"
-        img-src="../assets/image2.png"
-        img-alt="Image"
-        img-left
-        tag="article"
-        style="max-width: 20rem;"
-        class="mb-2"
-      >
-        <b-card-text>
-          Some quick example text to build on the card title and make up the bulk of the card's content.
-        </b-card-text>
-        <b-button href="#" variant="success">ComeÃ§ar Agora</b-button>
-      </b-card>
-    </div>
-
-    <div class="card_curso">
-      <b-card
-        title="IndependÃªncia Financeira"
-        img-src="../assets/image2.png"
-        img-alt="Image"
-        img-left
-        tag="article"
-        class="mb-2"
-      >
-        <b-card-text>
-          Some quick example text to build on the card title and make up the bulk of the card's content.
-        </b-card-text>
-        <b-button href="#" variant="success">ComeÃ§ar Agora</b-button>
-      </b-card>
-    </div>
+        <router-link class="course" to="/course"><b-button>Nossos Cursos</b-button></router-link>
+     </div>
   </div>
 </template>
 
 <script>
+import {mapActions, mapGetters} from 'vuex';
+
+export default {
+  
+  mounted(){
+    this.getCursos().then(()=>{
+      this.getAulas()
+    })
+
+    console.log('CURSOS', this.cursos)
+  },
+  methods:{
+    ...mapActions('cursos',['getCursos']),
+    ...mapActions('aulas',['getAulas'])
+  },
+  computed:{
+    ...mapGetters('cursos',['listCursos']),
+    ...mapGetters('aulas',['listAulas'])
+  }
+}
 </script>
 
+
 <style scoped>
-#curso-home{
+#home{
+  margin-top: 150px !important;
   margin: 0 auto;
   padding: 0 auto;
   text-align: center;
-  background: red;
-  float: right;
+  width: 60%;
+  height: 400px;
+  background: rgb(63, 158, 91);
+  color: #FFF;
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif bold;
+}
+
+#home p{
+  text-align: justify;
+  margin-left: 8px;
+  margin-right: 8px;
+}
+
+#home button{
+  background:rgb(202, 47, 137);
 }
 
 #curso-home b-button{
